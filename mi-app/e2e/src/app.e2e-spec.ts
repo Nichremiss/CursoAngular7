a4a9c5b5b2c2ae005acd97df236f8a6d229ab290
claudia <nichremiss@gmail.com>
2020-01-24 12:02:34 -0300

    Creación de primeros componentes

--- mi-app/e2e/src/app.e2e-spec.ts
+++ mi-app/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to mi-primera-app!');
+    expect(page.getTitleText()).toEqual('Welcome to mi-app!');
   });
 
   afterEach(async () => {
---
+++ mi-app/src/app/Personas/Personas.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-Personas',
+    templateUrl : './Personas.component.html'
+})
+export class PersonasComponent{
+nombre : 'Claudia Olivares';
+}
